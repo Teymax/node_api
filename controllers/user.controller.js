371d217798cdf8fed93e21a9b487880b27a17380
Den <datzyuk.den@gmail.com>
2019-06-25 10:50:21 +0300

    User model
    - added password hash
    - added compare method

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,19 +1,19 @@
-const { to, ReE, ReS } = require('../utils/requestHelpers');
+const { to, error, success } = require('../utils/requestHelpers');
 const authService = require('../services/auth.service');
 
 const create = async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   const { body } = req;
   if (!body.username && !body.email) {
-    return ReE(res, 'Please enter an email and username to register');
+    return error(res, 'Please enter an email and username to register');
   } else if (!body.password) {
-    return ReE(res, 'Please enter an password to register');
+    return error(res, 'Please enter an password to register');
   } else {
     let err, user;
     // place for auth service request
     [err, user] = await to(authService.register(body));
-    if (err) return ReE(res, err, 422);
-    return ReS(res, {message:'Successfully created new user.', user: user}, 201);
+    if (err) return error(res, err, 422);
+    return success(res, {message:'Successfully created new user.', user: user}, 201);
   }
 };
 
