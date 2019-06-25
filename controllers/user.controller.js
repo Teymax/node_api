7a72a29038d4c0015a7aef4bfbb7f908a1236c17
Andrew Oryn <thephantommr@gmail.com>
2019-06-25 17:59:51 +0300

    jwt and retrieving vehicles from db

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,20 +1,50 @@
-const { to, ReE, ReS } = require('../utils/requestHelpers');
+const { to, error, success } = require('../utils/requestHelpers');
 const authService = require('../services/auth.service');
+const jwt = require('jsonwebtoken');
+const { user } = require('../models');
+const secret = process.env.JWT_ENCRYPTION;
+const expiration = process.env.JWT_EXPIRATION;
 
 const create = async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   const { body } = req;
-  if (!body.username && !body.email) {
-    return ReE(res, 'Please enter an email and username to register');
+  if (!body.username || !body.email) {
+    return error(res, 'Please enter an email and username to register', 400);
   } else if (!body.password) {
-    return ReE(res, 'Please enter an password to register');
+    return error(res, 'Please enter an password to register', 400);
   } else {
     let err, user;
     // place for auth service request
     [err, user] = await to(authService.register(body));
-    if (err) return ReE(res, err, 422);
-    return ReS(res, {message:'Successfully created new user.', user: user}, 201);
+    if (err) return error(res, err, 422);
+    return success(res, {message:'Successfully created new user.', user: user}, 201);
+  }
+};
+
+const login = async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  const { body } = req;
+  const email = body.email;
+  const password = body.password;
+  if(email && password){
+    user.findOne({ where: {email: email} }).then((user)=>{
+      if(user.comparePassword(password)){
+        let access_token = jwt.sign({user_id: user.id}, secret, {expiresIn: 3600});
+        let refresh_token = jwt.sign({user_id: user.id, user_email: user.email}, secret, { expiresIn: expiration});
+        user.refresh_token = refresh_token;
+        user.save();
+        return success(res, {
+          user: user,
+          access_token: access_token,
+          refresh_token: refresh_token
+        }, 200);
+
+      }
+
+
+    })
   }
 };
 
 exports.create = create;
+exports.login = login;
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,10 +1,10 @@
 const { user } = require('../models');
-const { to, TE } = require('../utils/requestHelpers');
+const { to, throwError } = require('../utils/requestHelpers');
 
 const register = async userInfo => {
   let err, newUser;
   [err, newUser] = await to(user.create(userInfo));
-  if(err) TE('user already exists with that email');
+  if(err) throwError(err.message);
   return newUser;
 };
 
