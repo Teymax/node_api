fe0e4f6ef74904b0fa00919a85209447edcb4532
valery <valery.ptv@gmail.com>
2019-06-26 13:58:29 +0300

    styled vehicle delivery page

--- front-end/src/views/VehicleDelivery.vue
+++ front-end/src/views/VehicleDelivery.vue
@@ -1,16 +1,53 @@
 <template>
   <!-- <v-app> -->
   <div class="vehicle-delivery-container container">
+    <h2 class="page-header-text">Vehicle Delivery</h2>
     <v-card>
       <v-card-title>
-        Vehicles
+        Activity
         <v-spacer></v-spacer>
         <!-- <v-date-picker></v-date-picker> -->
-        <v-select class="table-select" :items="select" height="1" :width="10" label="Label" filled></v-select>
-        <v-text-field v-model="search" append-icon="search" class="table-search" label="Search"></v-text-field>
+
+        <v-menu
+          class="date-menu"
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="date"
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="290px"
+        >
+          <template class="date-slot" v-slot:activator="{ on }">
+            <v-text-field
+              class="date-text-field"
+              v-model="formattedDate"
+              label="mm/dd/yyyy"
+              prepend-icon="event"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker class="date-picker" v-model="date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn text color="primary" @click="saveDate">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
+
+        <v-select
+          class="table-select"
+          v-model="selectValue"
+          :items="selectData"
+          max-height="1"
+          width="10"
+          label="Search field"
+        ></v-select>
+        <v-text-field v-model="search" append-icon="search" class="table-search" label="Search..."></v-text-field>
       </v-card-title>
 
-      <v-data-table :headers="headers" :items="activities" :search="search" class="elevation-1">
+      <v-data-table :headers="headers" :items="activities" :search="search" class="elevation-1" ref="table">
         <template v-slot:items="props">
           <td class="text-xs-right photo-cell">
             <img alt="Car photo">
@@ -37,7 +74,8 @@ export default {
   name: "VehicleDelivery",
   data() {
     return {
-      select: [],
+      date: "",
+      menu: false,
       search: "",
       headers: [
         // {
@@ -160,34 +198,132 @@ export default {
           towing_company: "Marvin's Towing"
         }
       ]
-    };
+    }
+  },
+  methods: {
+    formatDate(date) {
+      if(!date) return null;
+
+      const [year, month, day] = date.split('-')
+
+      return `${month}/${day}/${year.slice(0, 2)}`
+    },
+    saveDate() {
+      this.$refs.menu.save(this.date);
+      
+      if (this.formattedDate) {
+        this.search = this.formattedDate
+      }
+
+      // this.$refs.table.search =  this.formattedDate;
+      // this.$refs.table.filter;
+      // console.log("TCL: saveDate -> this.$refs.table.filter", this.$refs.table.filter)
+    }
+  },
+  computed: {
+    selectData() {
+      return this.headers.map(item => item.text);
+    },
+    selectValue() {
+
+    },
+    tableSearch() {
+      return '';
+    },
+    formattedDate() {
+      let dateCopy = this.date; 
+      return this.formatDate(dateCopy);
+    }
   }
 };
 </script>
 
 <style lang="scss">
-
-.v-input {
-  /* flex: inherit; */
-}
+@import "../css/variables";
 
 .vehicle-delivery-container {
+
+  // .page-header {
+  //   font-size: 2.2rem;
+  //   font-weight: bold;
+  //   margin-bottom: 1.8rem;
+  // }
+
   // .table-select {
   //   flex: inherit;
   //   width: 10rem;
-  //   // height: 2rem;
   //   margin-right: 3rem;
   // }
 
-  // .table-select {
-  //   .v-select__selections {
-  //     padding: 0;
-  //   }
-  // }
+  .v-text-field .v-label--active {
+    transform: translateY(-26px) scale(0.75) !important;
+  }
 
-  // .table-search {
-  //   flex: inherit;
-  // }
+  .v-text-field {
+    margin-top: 0;
+  }
+
+  .table-select {
+    // margin-top: 13px;
+    width: 150px;
+    flex: initial;
+    box-sizing: border-box;
+  }
+
+  .table-select {
+    .v-input__slot {
+      background-color: $bg-third;
+      padding: 2px 10px;
+      height: 38px;
+      border-radius: 5px 0 0 5px;
+      border: 1px solid $border-primary;
+    }
+
+    .v-input__slot::before {
+      display: none;
+    }
+  }
+
+  .table-search {
+    flex: inherit;
+  }
+
+  .table-search {
+    .v-input__slot {
+      padding: 2px 10px;
+      border-radius: 0 5px 5px 0;
+      border: 1px solid $border-primary;
+      border-left: none;
+    }
+
+    .v-input__slot::before {
+      display: none;
+    }
+  }
+
+  .date-text-field {
+    position: relative;
+    flex: initial;
+    width: 150px;
+    margin-right: 1.5rem;
+  }
+
+  .date-text-field {
+    .v-input__slot {
+      padding: 2px 10px;
+      border-radius: 5px;
+      border: 1px solid $border-primary;
+    }
+
+    .v-input__slot::before {
+      display: none;
+    }
+
+    .v-input__prepend-outer {
+      position: absolute;
+      top: 15px;
+      right: 0;
+    }
+  }
 }
-
 </style>
