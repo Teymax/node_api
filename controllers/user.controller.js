54a7ac706be58fd2bbee3fa6c25330640d22a402
Andrew Oryn <thephantommr@gmail.com>
2019-06-26 17:31:51 +0300

    logout func & error handle

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,7 +13,7 @@ const create = async (req, res) => {
     let err, user;
     // place for auth service request
     [err, user] = await to(authService.register(body));
-    if (err) return error(res, err, 422);
+    if (err) return error(res, err.message, 400);
     return success(res, {message:'Successfully created new user.', user: user}, 201);
   }
 };
@@ -22,33 +22,16 @@ const login = async (req, res) => {
   const { body } = req;
   let err, user;
   [err, user] = await to(authService.login(body));
-  if (err) throwError(err.message);
-  success(res, user);
+  if (err) return  error(res, err.message, 400);
+  return success(res, user);
 };
 
-// const login = async (req, res) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   const { body } = req;
-//   const email = body.email;
-//   const password = body.password;
-//   if(email && password){
-//     await User.findOne({ where: {email: email} }).then(user =>{
-//       if(user.comparePassword(password)){
-//         let access_token = jwt.sign({user_id: user.id}, JWT_ENCRYPTION, {expiresIn: 3600})
-//             .catch(err => throwError(err.message, true));
-//         let refresh_token = jwt.sign({user_id: user.id, user_email: user.email}, JWT_ENCRYPTION, { expiresIn: JWT_EXPIRATION})
-//             .catch(err => throwError(err.message, true));
-//         user.refresh_token = refresh_token;
-//         user.save();
-//         return success(res, {
-//           user: user,
-//           access_token: access_token,
-//           refresh_token: refresh_token
-//         }, 200);
-//       }
-//     })
-//   }
-// };
+
+const logout = async (req, res) => {
+  req.logout();
+  return success(res, {message: 'Successfull logout.'}, 200);
+};
 
 exports.create = create;
 exports.login = login;
+exports.logout = logout;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,5 +4,5 @@ const UserController = require('../controllers/user.controller');
 
 router.post('/register', UserController.create);
 router.post('/login', UserController.login);
-
+router.post('/logout', UserController.logout);
 module.exports = router;
