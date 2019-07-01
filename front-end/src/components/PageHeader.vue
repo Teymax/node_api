4a4da164aeb09f39c046f24bd22615738898ff58
alexey100196 <alexeysobolev071@gmail.com>
2019-07-01 18:18:37 +0300

    work with settings

--- front-end/src/components/PageHeader.vue
+++ front-end/src/components/PageHeader.vue
@@ -7,38 +7,78 @@
       </div>
 
       <div class="user-block">
-        <div class="user-photo">
-          
-        </div>
+        <div class="user-photo"></div>
 
         <span class="user-name" d-inline-block>John Daniels</span>
+        <v-menu bottom left>
+          <template v-slot:activator="{ on }">
+            <v-btn dark icon v-on="on">
+              <v-icon>expand_more</v-icon>
+            </v-btn>
+          </template>
 
-        <v-icon @click="logout" color="white" class="icon-expand" primary>expand_more</v-icon>
+          <v-card light class="settings_bar pl-3" color="primary">
+            <v-list-item-avatar>
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>John Leider</v-list-item-title>
+            </v-list-item-content>
+
+            <v-layout row wrap>
+              <v-flex xs12>
+                <v-icon class>settings</v-icon>
+                <span @click="settingForm = true">Setting</span>
+              </v-flex>
+              <v-flex xs12>
+                <v-icon class>logout</v-icon>
+                <span>Log-out</span>
+              </v-flex>
+            </v-layout>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <!-- <v-btn text @click="menu = false">Cancel</v-btn>
+              <v-btn color="primary" text @click="menu = false">Save</v-btn>-->
+            </v-card-actions>
+          </v-card>
+        </v-menu>
       </div>
     </div>
+    <v-dialog max-width="600px" v-model="settingForm" transition="dialog-bottom-transition">
+      <setting-form @closeSettings="settingForm = false"></setting-form>
+    </v-dialog>
   </div>
 </template>
 
 <script>
 import actionTypes from "../store/action-types";
+import SettingForm from "./UserSettings.vue";
 import { mapActions } from "vuex";
 
 export default {
   name: "PageHeader",
+  data: () => ({
+    settingForm: false
+  }),
   computed: {
     // changes background color of header. It should blue on all pages instead of home page
     onHomePage() {
-      if(Object(this.$route) === this.$route && typeof this.$route.name === "string") {
-        return this.$route.name.indexOf("Home") !== -1
+      if (
+        Object(this.$route) === this.$route &&
+        typeof this.$route.name === "string"
+      ) {
+        return this.$route.name.indexOf("Home") !== -1;
       }
     }
   },
-  methods: { 
-
+  methods: {
     logout() {
       localStorage.removeItem("access-token");
       this.$router.push("/login");
     }
+  },
+  components: {
+    "setting-form": SettingForm
   }
 };
 </script>
@@ -47,7 +87,7 @@ export default {
 @import "../css/variables";
 
 .header-colorized {
-  background-color: #051D3E;
+  background-color: #051d3e;
 }
 
 .page-header {
@@ -103,6 +143,8 @@ export default {
   .icon-expand:hover {
     cursor: pointer;
   }
-
+}
+.settings_bar {
+  background: rgba(5, 29, 62, 0.92);
 }
 </style>
---
+++ front-end/src/components/UserSettings.vue
@@ -0,0 +1,73 @@
+<template>
+  <!-- <v-card>
+    <v-toolbar>
+      <v-toolbar-title>
+        Settings form      
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon @click="closeSettings()">close</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>-->
+  <v-card>
+    <v-toolbar dark flat color="primary">
+      <v-toolbar-title>Settings</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon dark @click="closeSettings">
+        <v-icon>close</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-card-text>
+      <v-list-item-avatar>
+        <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+      </v-list-item-avatar>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-toolbar-title>Change user name</v-toolbar-title>
+          <p>John Daniels</p>
+          <v-toolbar-title>Change password</v-toolbar-title>
+        </v-flex>
+        <v-layout row wrap>
+          <v-flex xs12 sm4>
+            <p>Old password</p>
+          </v-flex>
+          <v-flex xs12 sm8>
+            <v-text-field placeholder="Old password" solo></v-text-field>
+          </v-flex>
+
+          <v-flex xs12 sm4>
+            <p>New password</p>
+          </v-flex>
+          <v-flex xs12 sm8>
+            <v-text-field xs6 placeholder="New password" solo></v-text-field>
+          </v-flex>
+
+          <v-flex xs12 sm4>
+            <p>Confirm</p>
+          </v-flex>
+          <v-flex xs12 sm8>
+            <v-text-field placeholder="Confirm" solo></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "settings-form",
+  data: () => ({
+    settingForm: true
+  }),
+  methods: {
+    closeSettings() {
+      this.$emit("closeSettings");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
