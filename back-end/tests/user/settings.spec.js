8f614bdede0bd3b90d28802c5df2ea807ce213f4
Svyat <vls.svyat@gmail.com>
2019-07-04 18:03:01 +0300

    user settings tests done

--- back-end/tests/user/settings.spec.js
+++ back-end/tests/user/settings.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import {describe, it} from "mocha"
+import {user as User} from "../../models";
 
 chai.use(chaiHttp);
 chai.should();
@@ -46,7 +47,7 @@ describe('Settings', () => {
                     res.body.should.include.keys("refresh_token");
                     login.put("/user/update")
                         .set('Authorization', `Bearer ${res.body.access_token}`)
-                        .end((err, res)=>{
+                        .end((err, res) => {
                             res.should.have.status(400);
                             res.body.success.should.equal(false);
                             res.body.error.should.equal('no user found!');
@@ -71,14 +72,149 @@ describe('Settings', () => {
                     login.put("/user/update")
                         .set('Authorization', `Bearer ${res.body.access_token}`)
                         .send(usr)
-                        .end((err, res)=>{
+                        .end((err, res) => {
                             res.should.have.status(400);
                             res.body.success.should.equal(false);
                             res.body.error.should.equal('no user found!');
                             done()
                         });
                 });
-        })
+        });
+        it('PUT /user/update with correct mail', done => {
+            const user = {
+                email: 'test@gmail.com',
+                password: 'test'
+            };
+            let login = chai.request.agent(app);
+            login.post('/user/login')
+                .send(user)
+                .end((err, res) => {
+                    res.body.should.include.keys("access_token");
+                    res.body.should.include.keys("refresh_token");
+                    let usr = {
+                        email: 'test@gmail.com',
+                    };
+                    login.put("/user/update")
+                        .set('Authorization', `Bearer ${res.body.access_token}`)
+                        .send(usr)
+                        .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.success.should.equal(true);
+                            res.body.user.should.be.a('object');
+                            res.body.user.should.include.keys("refresh_token");
+                            done()
+                        });
+                });
+        });
+        it('PUT /user/update try to set new password without old password, with correct mail', done => {
+            const user = {
+                email: 'test@gmail.com',
+                password: 'test'
+            };
+            let login = chai.request.agent(app);
+            login.post('/user/login')
+                .send(user)
+                .end((err, res) => {
+                    res.body.should.include.keys("access_token");
+                    res.body.should.include.keys("refresh_token");
+                    let usr = {
+                        email: 'test@gmail.com',
+                        new_password: 'test1',
+                    };
+                    login.put("/user/update")
+                        .set('Authorization', `Bearer ${res.body.access_token}`)
+                        .send(usr)
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.success.should.equal(false);
+                            res.body.error.should.equal('Enter old password to change it');
+                            done()
+                        });
+                });
+        });
+        it('PUT /user/update try to set new password with invalid old password, with correct mail', done => {
+            const user = {
+                email: 'test@gmail.com',
+                password: 'test'
+            };
+            let login = chai.request.agent(app);
+            login.post('/user/login')
+                .send(user)
+                .end((err, res) => {
+                    res.body.should.include.keys("access_token");
+                    res.body.should.include.keys("refresh_token");
+                    let usr = {
+                        email: 'test@gmail.com',
+                        old_password: 'invalid',
+                        new_password: 'test1',
+                    };
+                    login.put("/user/update")
+                        .set('Authorization', `Bearer ${res.body.access_token}`)
+                        .send(usr)
+                        .end((err, res) => {
+                            console.log(res.body);
+                            res.should.have.status(400);
+                            res.body.success.should.equal(false);
+                            res.body.error.should.equal('invalid password');
+                            done()
+                        });
+                });
+        });
+        it('PUT /user/update try to set new password with correct old password, with correct mail', done => {
+            const user = {
+                email: 'test@gmail.com',
+                password: 'test'
+            };
+            let login = chai.request.agent(app);
+            login.post('/user/login')
+                .send(user)
+                .end((err, res) => {
+                    res.body.should.include.keys("access_token");
+                    res.body.should.include.keys("refresh_token");
+                    let usr = {
+                        email: 'test@gmail.com',
+                        old_password: 'test',
+                        new_password: 'test',
+                    };
+                    login.put("/user/update")
+                        .set('Authorization', `Bearer ${res.body.access_token}`)
+                        .send(usr)
+                        .end((err, res) => {
+                            console.log(res.body);
+                            res.should.have.status(200);
+                            res.body.success.should.equal(true);
+                            res.body.user.should.be.a('object');
+                            res.body.user.should.include.keys("refresh_token");
+                            done()
+                        });
+                });
+        });
+        it('PUT /user/update try to set new image with incorrect format, with correct mail', done => {
+            const user = {
+                email: 'test@gmail.com',
+                password: 'test'
+            };
+            let login = chai.request.agent(app);
+            login.post('/user/login')
+                .send(user)
+                .end((err, res) => {
+                    res.body.should.include.keys("access_token");
+                    res.body.should.include.keys("refresh_token");
+                    let usr = {
+                        email: 'test@gmail.com',
+                        user_image: 'test1',
+                    };
+                    login.put("/user/update")
+                        .set('Authorization', `Bearer ${res.body.access_token}`)
+                        .send(usr)
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.success.should.equal(false);
+                            res.body.error.should.equal('Enter old password to change it');
+                            done()
+                        });
+                });
+        });
     })
 
 });
